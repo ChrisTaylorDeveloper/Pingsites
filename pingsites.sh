3b5cef884dd56ca9e5c43075a879910b573f0d5a
Chris Taylor <chris@christaylordeveloper.co.uk>
2023-08-24 11:15:18 +0100

    Tabulate output.

--- pingsites.sh
+++ pingsites.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 pingsite () {
-    curl --output /dev/null --connect-timeout 5 --max-time 20 --write-out "%{http_code} - $2 $3 expected for $4 [$1]\n" -s -S "$1"
+    curl --output /dev/null --connect-timeout 5 --max-time 20 --write-out "%{http_code}" -s -S "$1"
 }
+
+tbl=$(printf "ACTUAL RESPONSE\tEXPECTED RESPONSE\tDEPLOYMENT\tURL")
 
 while read -r line; do
     url=$(echo "$line" | cut -d ',' -f 1)
-    response_code=$(echo "$line" | cut -d ',' -f 2)
-    response_description=$(echo "$line" | cut -d ',' -f 3)
-    deployment=$(echo "$line" | cut -d ',' -f 4)
-
-    pingsite "$url" "$response_code" "$response_description" "$deployment"
+    expected=$(echo "$line" | cut -d ',' -f 2)
+    deploy=$(echo "$line" | cut -d ',' -f 4)
+    actual=$(pingsite "$url")
+    tbl+=$'\n'$actual$'\t'$expected$'\t'$deploy$'\t'$url
 done
+
+echo "$tbl" | column -t -s $'\t'
